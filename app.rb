9f33ad363d0e6c7c3065a74bdbf228c43189624f
andreterceiro <andreapt82@gmail.com>
2015-11-02 20:28:02 +0000

    Finalizando a parte obrigatória do exercício

--- app.rb
+++ app.rb
@@ -43,7 +43,7 @@ class HangpersonApp < Sinatra::Base
     
     begin
       unless @game.guess letter
-        flash[:message] = "You already used that letter"
+        flash[:message] = "You have already used that letter"
       end
     rescue ArgumentError
       flash[:message] = "Invalid guess"
@@ -59,7 +59,15 @@ class HangpersonApp < Sinatra::Base
   # wrong_guesses and word_with_guesses from @game.
   get '/show' do
     ### YOUR CODE HERE ###
-    erb :show # You may change/remove this line
+    game_state = @game.check_win_or_lose
+
+    if game_state.to_s.eql?('win')
+      redirect "/win"
+    elsif game_state.to_s.eql?('lose')
+      redirect "/lose"
+    else
+      erb :show # You may change/remove this line
+    end
   end
   
   get '/win' do
