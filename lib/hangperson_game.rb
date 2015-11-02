1c6cde35d2598e1bcabcb788e698b44ec2ec1efa
andreterceiro <andreapt82@gmail.com>
2015-11-02 13:35:25 +0000

    Completando a classe HangpersonGame, com todos testes rspec dela passando

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -15,11 +15,54 @@ class HangpersonGame
     @wrong_guesses = ''
   end
 
+  def guess(letter)
+    raise ArgumentError.new if not (letter.instance_of? String)
+    letter.downcase!
+    raise ArgumentError.new if not (letter.match("^[a-z]$"))
+
+    return false if guessed_previously? letter
+
+    if word.include? letter
+      @guesses = @guesses + letter
+    else 
+      @wrong_guesses = @wrong_guesses + letter
+    end
+
+    true
+  end
+
+  def word_with_guesses
+    word_masked =''
+    @word.each_char do |char| 
+      if @guesses.include? char
+        word_masked = word_masked + char
+      else 
+        word_masked = word_masked + '-'
+      end
+    end
+
+    word_masked
+  end
+
+  def guessed_previously? letter
+    (@guesses.include? letter) or (@wrong_guesses.include? letter)
+  end
+
+  def check_win_or_lose
+    
+    if @word.eql? word_with_guesses
+      :win
+    elsif @wrong_guesses.length > 6
+      :lose
+    else 
+      :play
+    end
+  end
+
   def self.get_random_word
     require 'uri'
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
   end
-
 end
